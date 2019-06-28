39dbe978540a8abea0f6275d67ba61ce80c726e2
Kudratillo Ismatov <qudratillo95@gmail.com>
2019-06-28 21:11:37 +0500

    Auto-generated commit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ng build --prod --base-href "https://qudratillo.github.io/pager/"
+ng build --prod --base-href "https://qudratillo.github.io/pager/" --dir=dist/pager-web
 ngh --repo=https://github.com/Qudratillo/pager.git
